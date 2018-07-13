9c613726869501144981f47e027e7c3181af6d6a
Vladyslav Savko <vladyslav.savko@globallogic.com>
2018-07-13 15:59:49 +0300

    fix patch

--- test2.groovy
+++ test2.groovy
@@ -3,7 +3,7 @@ import groovy.json.JsonException
 import groovy.json.JsonSlurper
 
 
-def rootDirPatch = "/home/vsavko/Documents/dev/test2/src/API_TEST"
+def rootDirPatch = "/var/jenkins_home/workspace/pipeline/src/API_TEST"
 // def currentDir = new File("").getCanonicalPath()
 // def APIRootDir = currentDir + File.separator + "src" + File.separator + "API_TEST" + File.separator
 
