be6828075b60bc560cef70dcc7cf8f31d5ca4b95
Vladyslav Savko <vladyslav.savko@globallogic.com>
2018-07-13 15:31:37 +0300

    rootdir

--- test2.groovy
+++ test2.groovy
@@ -2,7 +2,7 @@ import groovy.json.JsonException
 import groovy.json.JsonSlurper
 
 
-def rootDirPatch = "/src/API_TEST"
+def rootDirPatch = "${rootDir}/src/API_TEST"
 def currentDir = new File("").getCanonicalPath()
 def APIRootDir = currentDir + File.separator + "src" + File.separator + "API_TEST" + File.separator
 
